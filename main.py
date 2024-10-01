f2fd6ef414a5df0236b454a2def835613c0fc070
AngJianHwee <megahwee@gmail.com>
2024-10-01 20:52:26 +0800

    Added training result plot

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from classifier import VECTORIZATION_APPROACHES, CLASSIFICATION_MODELS, run_model
+import json
+from tqdm import tqdm
+import random
 from vectorizer import simple_vocab_vectorizer, tfidf_vectorizer, word2vec_vectorizer, glove_vectorizer
 from preprocessing import nltk_init, preprocess_texts, generate_stopwords, generate_stemmer, generate_lemmatizer
-from utils import plot_figure
+from utils import plot_figure, plot_training_results
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -17,12 +21,17 @@ logger.setLevel(logging.INFO)
 logger.info('Logging started')
 logging.basicConfig(level=logging.INFO)
 
+logger.info('[Data] Start loading data')
 data = pd.read_csv(
     'https://raw.githubusercontent.com/Ankit152/IMDB-sentiment-analysis/master/IMDB-Dataset.csv')
+logger.info('[Data] Data loaded')
+
 data = data.reset_index(drop=True)  # drop index
 
-
+logger.info('[Data] Start plotting data')
 plot_figure(data)
+logger.info('[Data] Data plotted')
+
 
 nltk_init()
 
@@ -46,61 +55,41 @@ for i in range(10):
     print()
 
 
-# -----------------------------------------------------------------------------------
 # Approach 1: Simple vectorizer
 logger.info('[Vectorization] [Simple] Start vectorization')
 data['review_vector__simple'] = simple_vocab_vectorizer(data['review_cleaned'])
 logger.info('[Vectorization] [Simple] Vectorization completed')
 
-# -----------------------------------------------------------------------------------
 # Approach 2: tf-idf and save to review_vector__tfidf
 logger.info('[Vectorization] [tfidf] Start vectorization')
 data['review_vector__tfidf'] = tfidf_vectorizer(data['review_cleaned'])
 logger.info('[Vectorization] [tfidf] Vectorization completed')
 
-# -----------------------------------------------------------------------------------
 # approach 3: word2vec
 logger.info('[Vectorization] [word2vec] Start vectorization')
 data['review_vector__word2vec'] = word2vec_vectorizer(data['review_cleaned'])
 logger.info('[Vectorization] [word2vec] Vectorization completed')
 
-# -----------------------------------------------------------------------------------
 # Approach 4: GloVe
 logger.info('[Vectorization] [GloVe] Prepare GloVe start')
 data['review_vector__glove'] = glove_vectorizer(data['review_cleaned'])
 logger.info('[Vectorization] [GloVe] Vectorization completed')
 
-
-
+# view first 5 rows
 print(data.head())
 
 
-
-
-# -----------------------------------------------------------------------------------
-# -----------------------------------------------------------------------------------
-# -----------------------------------------------------------------------------------
-# train test split add label save to column "train_test_flag"
-import random
-
-# random split by 8:2
+# random split by 8:2, add train_test_flag
 index = ['train'] * int(len(data) * 0.8) + ['test'] * \
     (len(data) - int(len(data) * 0.8))
 random.shuffle(index)
 data['train_test_flag'] = index
-# -----------------------------------------------------------------------------------
-from tqdm import tqdm
-import json
 
-
+# start training
 train_results = {}
-
-
-from classifier import VECTORIZATION_APPROACHES, CLASSIFICATION_MODELS, run_model
-
 for approach in tqdm(VECTORIZATION_APPROACHES, desc='Approach', position=0):
     train_results[approach] = {}
-    
+
     # get train and test data from previous step
     X_train = data[data['train_test_flag'] == 'train'][approach].values
     X_test = data[data['train_test_flag'] == 'test'][approach].values
@@ -112,9 +101,10 @@ for approach in tqdm(VECTORIZATION_APPROACHES, desc='Approach', position=0):
     y_test = [1 if x == 'positive' else 0 for x in y_test]
 
     for cur_model in tqdm(CLASSIFICATION_MODELS, desc='Model', position=1):
-        
-        train_results[approach][cur_model[1]] = run_model(cur_model, approach, X_train, y_train, X_test, y_test,logger)
-        
+
+        train_results[approach][cur_model[1]] = run_model(
+            cur_model, approach, X_train, y_train, X_test, y_test, logger)
+
         acc, f1, precision, recall = train_results[approach][cur_model[1]].values()
         logger.info(f'[Predict] [approach: {approach}] [Model: {cur_model[1]}] Completed, acc: {acc:.4f}, f1: {f1:.4f}, precision: {precision:.4f}, recall: {recall:.4f}')
 
@@ -125,4 +115,6 @@ with open('train_results.json', 'w') as f:
     json.dump(train_results, f)
 logger.info('[Dump] Dumped train results to ./train_results.json')
 
-print(train_results)
+logger.info('[Plot] Start plotting for training results')
+plot_training_results(train_results)
+logger.info('[Plot] Training results plotted')
--- utils.py
+++ utils.py
@@ -1,12 +1,43 @@
 from matplotlib import pyplot as plt
 
+APPROACH_NAME_DICT = {
+        'review_vector__simple': 'Simple Vectorizer',
+        'review_vector__tfidf': 'TF-IDF',
+        'review_vector__word2vec': 'Word2Vec',
+        'review_vector__glove': 'GloVe',
+    }
 
 def plot_figure(data):
+    plt.figure()
     data['sentiment'].value_counts().plot(
         kind='bar', title='Distribution of Sentiment', figsize=(5, 3))
     plt.xticks(rotation=0)
     plt.savefig('sentiment_distribution.png')
 
+    plt.figure()
     data['review'].apply(len).plot(kind='hist', title='Review Length Distribution',
                                    bins=50, figsize=(5, 3))
     plt.savefig('review_length_distribution.png')
+
+
+def plot_training_results(train_results):
+    plt.figure()
+    approaches = list(train_results.keys())
+
+    models = list(train_results[approaches[0]].keys())
+
+    
+
+    # get accuracy for each model, then plot with .plot, legend is different approach
+    # plot together on same plot
+    for approach in approaches:
+        accs = [train_results[approach][model]['accuracy'] for model in models]
+        plt.plot(models, accs, label=APPROACH_NAME_DICT[approach], marker='x')
+
+    plt.gcf().set_size_inches(10, 5)
+    plt.legend()
+    plt.xticks(rotation=45)
+    plt.ylim(0.5, 1)
+    plt.grid()
+    plt.title('Accuracy of Different Models')
+    plt.savefig('accuracy_of_different_models.png')
